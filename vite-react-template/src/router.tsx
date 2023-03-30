c466e04de54844aadca204a73d14beadc409a32e
ljylwnxx <ljylwnxx@163.com>
2023-03-30 15:03:35 +0800

    feat:router

---
+++ vite-react-template/src/router.tsx
@@ -0,0 +1,23 @@
+import {Suspense}, React from 'react'
+import type { RouteObject } from 'react-router-dom';
+import Index from './pages/index/index';
+
+// React.lazy 配合 import() 实现懒加载
+const About = React.lazy(() => import('./pages/about'));
+
+const routes: RouteObject[] = [
+  {
+    path: '/',
+    element: <Index />,
+  },
+  {
+    path: '/about',
+    element: (
+      <Suspense fallback={<span>loading component</span>}>
+        <About />
+      </Suspense>
+    ),
+  },
+];
+
+export default routes;
